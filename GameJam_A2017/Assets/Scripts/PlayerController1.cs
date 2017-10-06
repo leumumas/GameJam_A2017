640f30878927c31521005c8066c4950e0fda93e7
Patrick Levesque <levesque_patrick_2@hotmail.com>
2017-10-06 17:46:08 -0400

    tk j'essaye

---
+++ GameJam_A2017/Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    bool facingLeft = false;
+    bool facingDown = false;
+    bool facingUp = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float Hmove = Input.GetAxis("Horizontal");
+        float Vmove = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Hmove * maxSpeed, Vmove * maxSpeed);
+	}
+}
