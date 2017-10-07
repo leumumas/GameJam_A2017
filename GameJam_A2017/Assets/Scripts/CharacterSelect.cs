58bfb74cfe0ef7544b2f980a354b033f287cbe33
Patrick Levesque <levesque_patrick_2@hotmail.com>
2017-10-07 05:37:25 -0400

    Character selection

---
+++ GameJam_A2017/Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour {
+
+    private int player1, player2;
+    private GameObject _buttons1, _buttons2, _continue, _p1, _p2;
+
+	// Use this for initialization
+	void Start () {
+        _buttons1 = GameObject.Find("Buttons1");
+        _buttons2 = GameObject.Find("Buttons2");
+        _continue = GameObject.Find("Continue");
+        _p1 = GameObject.Find("P1");
+        _p2 = GameObject.Find("P2");
+
+        _buttons1.SetActive(true);
+        _buttons2.SetActive(false);
+        _continue.SetActive(false);
+        _p1.SetActive(true);
+        _p2.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Flosh()
+    {
+        _buttons1.SetActive(false);
+        _buttons2.SetActive(true);
+        _continue.SetActive(false);
+        _p1.SetActive(false);
+        _p2.SetActive(true);
+
+        player1 = 0;
+    }
+
+    public void Mentalist()
+    {
+        _buttons1.SetActive(false);
+        _buttons2.SetActive(true);
+        _continue.SetActive(false);
+        _p1.SetActive(false);
+        _p2.SetActive(true);
+
+        player1 = 2;
+    }
+
+    public void ShadowKitty()
+    {
+        _buttons1.SetActive(false);
+        _buttons2.SetActive(true);
+        _continue.SetActive(false);
+        _p1.SetActive(false);
+        _p2.SetActive(true);
+
+        player1 = 1;
+    }
+
+    public void Flosh1()
+    {
+        _buttons1.SetActive(false);
+        _buttons2.SetActive(false);
+        _continue.SetActive(true);
+        _p1.SetActive(false);
+        _p2.SetActive(false);
+
+        player2 = 0;
+    }
+
+    public void Mentalist1()
+    {
+        _buttons1.SetActive(false);
+        _buttons2.SetActive(false);
+        _continue.SetActive(true);
+        _p1.SetActive(false);
+        _p2.SetActive(false);
+
+        player2 = 2;
+    }
+
+    public void ShadowKitty1()
+    {
+        _buttons1.SetActive(false);
+        _buttons2.SetActive(false);
+        _continue.SetActive(true);
+        _p1.SetActive(false);
+        _p2.SetActive(false);
+
+        player2 = 1;
+    }
+}
--- GameJam_A2017/Assets/Scripts/Menu.cs
+++ GameJam_A2017/Assets/Scripts/Menu.cs
@@ -6,32 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
-    private GameObject _main, _options;
+    private GameObject _main, _options, _characSelect;
 
 	// Use this for initialization
 	private void Start () {
         _main = GameObject.Find("Main");
         _options = GameObject.Find("Options");
+        _characSelect = GameObject.Find("Character");
 
         _main.SetActive(true);
         _options.SetActive(false);
+        _characSelect.SetActive(false);
 	}
 
     public void Back()
     {
         _main.SetActive(true);
         _options.SetActive(false);
+        _characSelect.SetActive(false);
     }
 
    public void Play()
     {
-
+        _main.SetActive(false);
+        _options.SetActive(false);
+        _characSelect.SetActive(true);
     }
 
     public void Options()
     {
         _main.SetActive(false);
         _options.SetActive(true);
+        _characSelect.SetActive(false);
     }
 
     public void Exit()
