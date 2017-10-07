19f94cd94e6159d5819892291eacc228ec5e02ec
Patrick Levesque <levesque_patrick_2@hotmail.com>
2017-10-06 22:54:05 -0400

    Le vrai soleil on l'a pas encore vu

---
+++ GameJam_A2017/Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    private GameObject _main, _options;
+
+	// Use this for initialization
+	void Start () {
+        _main = GameObject.Find("Main");
+        _options = GameObject.Find("Options");
+
+        _main.SetActive(true);
+        _options.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Back()
+    {
+        _main.SetActive(true);
+        _options.SetActive(false);
+    }
+
+   public void Play()
+    {
+
+    }
+
+    public void Options()
+    {
+        _main.SetActive(false);
+        _options.SetActive(true);
+        print("allo");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
