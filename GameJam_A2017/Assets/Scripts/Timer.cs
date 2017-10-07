ca921f04099de357de6a930c28d4f7f1ab130f5a
Vostro3560\Utilisateur <noelson_melville@bell.net>
2017-10-06 22:57:00 -0400

    Script du timer avec ajout du générateur de temps aléatoire.

--- GameJam_A2017/Assets/Scripts/Timer.cs
+++ GameJam_A2017/Assets/Scripts/Timer.cs
@@ -5,21 +5,22 @@ using UnityEngine.UI;
 public class timer : MonoBehaviour
 {
     public Text timerText;
-    public float timeRemaining = 30;
-    //Random random = 0;//
-    AudioSource Bruh;
-    //private AudioSource playAudio;//
+    public float timeRemaining = 60;
+    int time;
+    
 
     // Use this for initialization
     void Start()
     {
         //Random(timeRemaining);//
+        time = Random.Range(30,60);
         timerText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       // return obj.().ToString();//
         timeRemaining -= Time.deltaTime; //countdown
         timerText.text = timeRemaining.ToString("f2"); //"f2", pour les décimals
         print(timeRemaining);
