6e21cfa50b36c76bd6b4654498595b4162ef54e7
unknown <samuellalibel@hotmail.com>
2017-10-06 20:29:43 -0400

    Début des maisons et de la map

---
+++ GameJam_A2017/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public int victoryCondition = 10;
+	private static GameManager hanzo = null;
+	public PlayerController1 player1;
+	private Map worldMap;
+	//public PlayerController2 player2;
+
+	void Awake() {
+		hanzo = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		worldMap = Map.Instance;
+		Map m = worldMap;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static GameManager Instance
+	{
+		get
+		{
+			return hanzo;
+		}
+
+	}
+}
---
+++ GameJam_A2017/Assets/Scripts/House.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour {
+
+	int Type = 1;
+	//private Timer time;
+
+
+	// Use this for initialization
+	void Start () {
+		Map.Instance.addHouses (this);
+		typeDefine (Type);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void typeDefine (int t) {
+		switch (t) {
+		case 0: 
+			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite>("Pauvre");
+				break;
+		case 1: 
+			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite>("moyen");
+				break;
+		case 2: 
+			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite>("Luxe");
+				break;
+		case 3: 
+			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite>("RAF");
+				break;
+		}	
+	}
+}
