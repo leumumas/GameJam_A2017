1e7ff834f9187762244ba3792529c816f47aa6ac
Vostro3560\Utilisateur <noelson_melville@bell.net>
2017-10-06 22:20:20 -0400

    Script du timer, supposer générer un timer au "spawn" d'une maison.

---
+++ GameJam_A2017/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿//Code pour un Count Down
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    public float timeRemaining = 30;
+    //Random random = 0;//
+    AudioSource Bruh;
+    //private AudioSource playAudio;//
+
+    // Use this for initialization
+    void Start()
+    {
+        //Random(timeRemaining);//
+        timerText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime; //countdown
+        timerText.text = timeRemaining.ToString("f2"); //"f2", pour les décimals
+        print(timeRemaining);
+
+        if (timeRemaining <= 0) //Détruire le "gameObjet" attaché si le compteur arrive à 0:00
+        {
+            //playAudio = Bruh;//
+            Destroy(gameObject);
+        }
+    }
+
+
+}
