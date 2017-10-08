cb3862b3195fda5cba689ce05b77e12ea3b994a3
Patrick Levesque <levesque_patrick_2@hotmail.com>
2017-10-08 07:43:28 -0400

    j'avais pas sauvegarder oupsi

--- GameJam_A2017/Assets/Scripts/Game.cs
+++ GameJam_A2017/Assets/Scripts/Game.cs
@@ -5,14 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour {
 
-    public int typePlayer1, typePlayer2;
-    CharacterSelect player1, player2;
 
 	// Use this for initialization
 	void Start () {
-        typePlayer1 = player1.Player1();
-        typePlayer2 = player2.Player2();
-        DontDestroyOnLoad(gameObject);
+        
 	}
 	
 	// Update is called once per frame
