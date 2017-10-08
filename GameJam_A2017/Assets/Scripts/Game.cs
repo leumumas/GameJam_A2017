4b82316a508686eaeb4029e7c3e944d07c808c8a
Patrick Levesque <levesque_patrick_2@hotmail.com>
2017-10-08 07:38:35 -0400

    Règle le bug du transfert de type de perso

---
+++ GameJam_A2017/Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+    public int typePlayer1, typePlayer2;
+    CharacterSelect player1, player2;
+
+	// Use this for initialization
+	void Start () {
+        typePlayer1 = player1.Player1();
+        typePlayer2 = player2.Player2();
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Samuel");
+    }
+}
